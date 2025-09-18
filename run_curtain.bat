2aa5d19a36520f23744609960f4eb7162f2889b5
vincent13 <vincentheymans@gmail.com>
2025-09-18 15:32:59 +0200

    run_curtain.bat updated with npm start command in background

--- run_curtain.bat
+++ run_curtain.bat
@@ -1,5 +1,14 @@
 @echo off
 cd /d "C:\Java\Projects\personal projects\gridster-trial"
+
+:: Start Next.js silently in background
+start /b npm run start
+
+:: Wait 5 seconds for server to boot
+timeout /t 5 >nul
+
+:: Start Electron curtain
 npm run electron
+
 echo Exit Code: %errorlevel%
 pause
